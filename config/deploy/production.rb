8cad2b675ec01c4c965cc0aa34f9fe2cf2388e3d
Ubuntu <ubuntu@ip-172-31-12-226.us-east-2.compute.internal>
2017-06-29 04:52:51 +0000

    Fix production IP

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,5 +1,5 @@
 set :user, "danbooru"
 set :rails_env, "production"
-server "sonohara.donmai.us", :roles => %w(web app db), :primary => true, :user => "danbooru"
+server "ec2-13-59-92-103.us-east-2.compute.amazonaws.com", :roles => %w(web app db), :primary => true, :user => "danbooru"
 
 set :linked_files, fetch(:linked_files, []).push(".env.production")
